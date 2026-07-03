51a70b9beaf72ec82f6f6ea21a06c1790e338b5d
Campenito <raulrojochico@gmail.com>
2026-07-03 13:29:45 +0200

    Delete first 12 lines

--- joke.py
+++ joke.py
@@ -1,16 +1,3 @@
-"""Run this model in Python
-
-> pip install openai
-"""
-import os
-from openai import OpenAI
-
-# To authenticate with the model you will need to generate a personal access token (PAT) in your GitHub settings. 
-# Create your PAT token by following instructions here: https://docs.github.com/en/authentication/keeping-your-account-and-data-secure/managing-your-personal-access-tokens
-client = OpenAI(
-    base_url="https://models.github.ai/inference",
-    api_key=os.environ["GITHUB_TOKEN"],
-)
 
 response = client.chat.completions.create(
     messages=[
@@ -20,7 +7,7 @@ response = client.chat.completions.create(
         },
         {
             "role": "user",
-            "content": "Explain this joke, I was going to visit my family on May 3rd. My mom said that it was a great idea as my dad's poetry reading is that night. So now I'm flying in on May 4th.",
+            "content": "Explain this joke, I was going to visit my family on May 3rd. My mom said that it was a great idea as my dad's poetry reading is that night. So now I'm flying in on May 4th[...]
         }
     ],
     model="openai/gpt-4o-mini",
